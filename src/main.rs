5ab2451ef77d8d29301cc670b50eeda9a7b05082
FieldofClay <7278759+FieldofClay@users.noreply.github.com>
2024-02-18 17:22:58 +1100

    Improve Result & Option handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use hyprland::data::{Monitors, Workspace, Workspaces};
 use hyprland::event_listener::EventListenerMutable as EventListener;
-use hyprland::shared::HyprData;
+use hyprland::shared::{HyprData,HyprError};
 use hyprland::shared::HyprDataActive;
 use hyprland::Result;
 use std::env;
 use serde::Serialize;
 use serde_json::json;
+use std::sync::Arc;
 
 const HELP: &str = "\
 hyprland-workspaces: a multi monitor aware hyprland workspaces json widget generator for eww/waybar.
@@ -35,28 +36,32 @@ struct MonitorCustom {
     pub workspaces: Vec<WorkspaceCustom>
 }
 
-fn get_workspace_windows(monitor: &str)-> Vec<WorkspaceCustom>  {
+fn get_workspace_windows(monitor: &str)-> Result<Vec<WorkspaceCustom>>  {
     // get all workspaces
-    let mut workspaces: Vec<_> = Workspaces::get().expect("unable to get workspaces").into_iter().collect();
+    let mut workspaces: Vec<_> = Workspaces::get()?
+        .into_iter()
+        .collect();
     workspaces.sort_by_key(|w| w.id);
 
     //get active workspace
     let active_workspace_id: i32;
     if monitor == "ALL" {
-        active_workspace_id = Workspace::get_active().expect("unable to get active workspace").id;
+        active_workspace_id = Workspace::get_active()?.id;
     } else {
-        active_workspace_id = Monitors::get()
-            .expect("unable to get monitors")
+        active_workspace_id = Monitors::get()?
             .find(|m| m.name == monitor)
-            .unwrap()
+            .ok_or_else(|| {
+                HyprError::NotOkDispatch("No monitor found".to_string())
+            })?
             .active_workspace
             .id;
     }
     //active monitor name
-    let active_monitor_name = Monitors::get()
-        .expect("unable to get monitors")
+    let active_monitor_name = Monitors::get()?
         .find(|m| m.focused == true)
-        .unwrap()
+        .ok_or_else(|| {
+            HyprError::NotOkDispatch("No active monitor found".to_string())
+        })?
         .name;
     let mut out_workspaces: Vec<WorkspaceCustom> = Vec::new();
 
@@ -76,24 +81,29 @@ fn get_workspace_windows(monitor: &str)-> Vec<WorkspaceCustom>  {
             };
             out_workspaces.push(ws);
     }
-    return out_workspaces;
+    return Ok(out_workspaces);
 }
+
+fn get_all_advanced()-> Result<Vec<MonitorCustom>>  {
+    let monitors = Monitors::get()?;
+    let mut out_monitors: Vec<MonitorCustom> = Vec::new();
+    for m in monitors {
+        let workspaces = get_workspace_windows(&m.name)?;
+        let mc: MonitorCustom = MonitorCustom {
+            name: m.name,
+            workspaces: workspaces,
+        };
+        out_monitors.push(mc);
+    }
+    Ok(out_monitors)
+}
+
 
 fn output(monitor: &str) {
     if monitor == "_" {
-        let monitors = Monitors::get().expect("unable to get monitors");
-        let mut out_monitors: Vec<MonitorCustom> = Vec::new();
-        for m in monitors {
-            let workspaces = get_workspace_windows(&m.name);
-            let mc: MonitorCustom = MonitorCustom {
-                name: m.name,
-                workspaces: workspaces,
-            };
-            out_monitors.push(mc);
-        }
-        println!("{}", json!(out_monitors).to_string());
+        println!("{}", json!(get_all_advanced().unwrap_or_default()).to_string());
     } else {
-        println!("{}", json!(get_workspace_windows(monitor)).to_string());
+        println!("{}", json!(get_workspace_windows(monitor).unwrap_or_default()).to_string());
     }
 }
 
@@ -105,72 +115,83 @@ fn main() -> Result<()> {
         println!("{HELP}");
         std::process::exit(0);
     }
-    let mon = env::args().nth(1).unwrap();
-    if let None = Monitors::get()
-        .expect("unable to get monitors")
-        .find(|m| m.name == mon || mon == "ALL" || mon == "_") {
+    let mon = Arc::new(String::from(args[1].to_string()));
+    if let None = Monitors::get()?
+        .find(|m| m.name == mon.to_string() || mon.to_string() == "ALL" || mon.to_string() == "_") {
             println!("Unable to find monitor {mon}");
             std::process::exit(0);
     }
 
-    macro_rules! output {
-        () => {
-            output(&env::args().nth(1).unwrap());
-        };
-    }
-    output!();
+    output(&mon);
     // Create a event listener
     let mut event_listener = EventListener::new();
-    event_listener.add_workspace_change_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_workspace_change_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_workspace_added_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_workspace_added_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_workspace_destroy_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_workspace_destroy_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_workspace_moved_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_workspace_moved_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_monitor_added_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_monitor_added_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_monitor_removed_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_monitor_removed_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_window_close_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_window_close_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_window_open_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_window_open_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_active_monitor_change_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_active_monitor_change_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_active_window_change_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_active_window_change_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_window_close_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_window_close_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_fullscreen_state_change_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_fullscreen_state_change_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_window_moved_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_window_moved_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_layer_open_handler(|_, _| {
-        output!();
-     });
-    event_listener.add_layer_closed_handler(|_, _| {
-        output!();
-     });
-    event_listener.add_urgent_state_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_layer_open_handler(move |_, _| {
+        output(&mon_clone);
     });
-    event_listener.add_window_title_change_handler(|_, _| {
-        output!();
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_layer_closed_handler(move |_, _| {
+        output(&mon_clone);
+    });
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_urgent_state_handler(move |_, _| {
+        output(&mon_clone);
+    });
+    let mon_clone = Arc::clone(&mon);
+    event_listener.add_window_title_change_handler(move |_, _| {
+        output(&mon_clone);
     });
 
     event_listener.start_listener()
